91a4df9f0a75c7d9040f94beb2886bbb2972ea61
CertifiedForkliftDriver <124921581+CertifiedForkliftDriver@users.noreply.github.com>
2024-05-13 09:58:33 -0700

    added users and react components

---
+++ leon_website/src/metric_tracker.py
@@ -0,0 +1,17 @@
+import time
+import csv
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from popUsers import user3 as user
+
+
+# Initialize browser
+driver = webdriver.Chrome()
+
+# Navigate to your website 
+driver.get("http://localhost:3000/")
+
+user.userAction(driver)
+
+# Close the browser
+driver.quit()
